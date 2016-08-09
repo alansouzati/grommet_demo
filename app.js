beec4c7d147fc667c668efccb6791d04c70a01e6
Nicolas Medina Sanchez <nicoms91@gmail.com>
2016-08-09 15:50:42 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,28 +6,13 @@ import {checkLogStatus, logOut} from '../actions/';
 import {bindActionCreators} from 'redux';
 import {App} from 'grommet';
 
-class Main extends Component {
-
-  componentDidMount(){
-  	this.props.checkLogStatus();
-  }		
+export default class Main extends Component {
 
   render() {
     return (
     <App centered={true} className="_main">
-      <Navbar logStatus={this.props.logStatus} logOut={this.props.logOut.bind(this)}/>
-	    {this.props.children}
+	
     </App>
     );
   }
 }
-
-function mapStateToProps(state){
-    return {logStatus: state.logStatus};
-}
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({checkLogStatus, logOut}, dispatch);
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
