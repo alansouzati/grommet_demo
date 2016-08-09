e078701577b2f51b34ffc2011db96454c01c9453
Nicolas Medina Sanchez <nicoms91@gmail.com>
2016-08-09 15:50:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {Router, hashHistory} from 'react-router';
-import routes from './routes';
 import reducers from './reducers';
 import ReduxPromise from 'redux-promise';
+import App from './app';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={hashHistory} routes={routes} />
+    <App />
   </Provider>
   , document.querySelector('.container'));
